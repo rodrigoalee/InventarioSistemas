9f1f329eba925b42b77d310ac15cc84da6e3a9bc
rodrigoalee <rooodrigojc@gmail.com>
2026-03-24 14:48:50 -0600

    Rodrigo Juárez: Primer Update de Prueba.

--- Inventario.Sis/obj/Debug/net6.0/Inventario.Sis.AssemblyInfo.cs
+++ Inventario.Sis/obj/Debug/net6.0/Inventario.Sis.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Inventario.Sis")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8eb3c5b4cd55a2ca3d316ea02dbc94b95e76fe13")]
 [assembly: System.Reflection.AssemblyProductAttribute("Inventario.Sis")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Inventario.Sis")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
